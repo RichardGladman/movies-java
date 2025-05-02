986b83d34751432efc8acf78aa73f2b63f64abd3
Richard Gladman <richard@thefifthcontinent.com>
2025-05-02 19:12:41 +0100

    Add ANSI Codes to view

---
+++ app/src/main/java/com/thefifthcontinent/movies/view/ANSICodes.java
@@ -0,0 +1,61 @@
+package com.thefifthcontinent.movies.view;
+
+public class ANSICodes
+{
+	private static String BLACK = "\033[30m";
+	private static String RED = "\033[31m";
+	private static String GREEN = "\033[32m";
+	private static String YELLOW = "\033[33m";
+	private static String BLUE = "\033[34m";
+	private static String MAGENTA = "\033[35m";
+	private static String CYAN = "\033[36m";
+	private static String WHITE = "\033[37m";
+	private static String DEFAULT_COLOUR = "\033[39m";
+
+	private static String BLACK_BACKGROUND = "\033[40m";
+	private static String RED_BACKGROUND = "\033[41m";
+	private static String GREEN_BACKGROUND = "\033[42m";
+	private static String YELLOW_BACKGROUND = "\033[43m";
+	private static String BLUE_BACKGROUND = "\033[44m";
+	private static String MAGENTA_BACKGROUND = "\033[45m";
+	private static String CYAN_BACKGROUND = "\033[46m";
+	private static String WHITE_BACKGROUND = "\033[47m";
+	private static String DEFAULT_BACKGROUND = "\033[49m";
+
+	private static String BLACK_BRIGHT = "\033[90m";
+	private static String RED_BRIGHT = "\033[91m";
+	private static String GREEN_BRIGHT = "\033[92m";
+	private static String YELLOW_BRIGHT = "\033[93m";
+	private static String BLUE_BRIGHT = "\033[94m";
+	private static String MAGENTA_BRIGHT = "\033[95m";
+	private static String CYAN_BRIGHT = "\033[96m";
+	private static String WHITE_BRIGHT = "\033[97m";
+	private static String DEFAULT_BRIGHT = "\033[99m";
+
+	private static String BLACK_BACKGROUND_BRIGHT = "\033[100m";
+	private static String RED_BACKGROUND_BRIGHT = "\033[101m";
+	private static String GREEN_BACKGROUND_BRIGHT = "\033[102m";
+	private static String YELLOW_BACKGROUND_BRIGHT = "\033[103m";
+	private static String BLUE_BACKGROUND_BRIGHT = "\033[104m";
+	private static String MAGENTA_BACKGROUND_BRIGHT = "\033[105m";
+	private static String CYAN_BACKGROUND_BRIGHT = "\033[106m";
+	private static String WHITE_BACKGROUND_BRIGHT = "\033[107m";
+	private static String DEFAULT_BACKGROUND_BRIGHT = "\033[109m";
+
+	private static String BOLD = "\033[1m";
+	private static String BOLD_OFF = "\033[21m";
+	private static String DIM = "\033[2m";
+	private static String DIM_OFF = "\033[21m";
+	private static String UNDERLINE = "\033[4m";
+	private static String UNDERLINE_OFF = "\033[24m";
+	private static String BLINK = "\033[5m";
+	private static String BLINK_OFF = "\033[25m";
+	private static String REVERSE = "\033[7m";
+	private static String REVERSE_OFF = "\033[27m";
+	private static String HIDE = "\033[8m";
+	private static String HIDE_OFF = "\033[28m";
+
+	private static String RESET = "\033[0m";
+
+	private static String CLEAR_SCREEN = "\033[2J";
+}
