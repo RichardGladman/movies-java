e5c2c04eed09cf3856cac63244b263c67fb53c7c
Richard Gladman <richard@thefifthcontinent.com>
2025-05-05 08:33:35 +0100

    Create director controller and add director.

--- app/src/main/java/com/thefifthcontinent/movies/App.java
+++ app/src/main/java/com/thefifthcontinent/movies/App.java
@@ -4,16 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.thefifthcontinent.movies.controllers.ActorController;
+import com.thefifthcontinent.movies.controllers.DirectorController;
 import com.thefifthcontinent.movies.controllers.MovieController;
 import com.thefifthcontinent.movies.menu.Menu;
 import com.thefifthcontinent.movies.menu.Option;
 import com.thefifthcontinent.movies.model.Actor;
+import com.thefifthcontinent.movies.model.Director;
 import com.thefifthcontinent.movies.model.Movie;
 
 public class App 
 {
 	private static final Map<String, Movie> movies = new HashMap<>();
 	private static final Map<String, Actor> actors = new HashMap<>();
+	private static final Map<String, Director> directors = new HashMap<>();
 	
 	public static Map<String, Movie> getMovies()
 	{
@@ -23,6 +26,11 @@ public class App
 	public static Map<String, Actor> getActors()
 	{
 		return actors;
+	}
+	
+	public static Map<String, Director> getDirectors()
+	{
+		return directors;
 	}
 	
     public static void main(String[] args) 
@@ -55,13 +63,18 @@ public class App
     {
     	new ActorController().run();
     }
+    
+    private void manageDirectors()
+    {
+    	new DirectorController().run();
+    }
      
     private Menu createMenu()
     {
     	Menu menu = new Menu("Main Menu");
     	menu.addOption(new Option("Manage Movies", 'M', this::manageMovies));
     	menu.addOption(new Option("Manage Actors", 'A', this::manageActors));
-    	menu.addOption(new Option("Manage Directors", 'D', null));
+    	menu.addOption(new Option("Manage Directors", 'D', this::manageDirectors));
     	menu.addOption(new Option("Quit", 'Q', null));
 
     	return menu;
---
+++ app/src/main/java/com/thefifthcontinent/movies/controllers/DirectorController.java
@@ -0,0 +1,55 @@
+package com.thefifthcontinent.movies.controllers;
+
+import com.thefifthcontinent.movies.App;
+import com.thefifthcontinent.movies.menu.Menu;
+import com.thefifthcontinent.movies.menu.Option;
+import com.thefifthcontinent.movies.model.Director;
+import com.thefifthcontinent.movies.view.View;
+
+public class DirectorController
+{
+	private final View view = new View();
+	private boolean dataChanged;
+    
+    public void run()
+    {
+    	Menu menu = createMenu();
+    	Option choice = null;
+    	
+    	do {
+			menu.render();
+			choice = menu.getChoice();
+			
+			if (choice != null && choice.getAction() != null) {
+				choice.getAction().run();
+			}
+			
+		} while (choice.getOption() != 'B');
+    }
+    
+    private void add()
+    {
+    	view.header("Adding Director");
+    	
+    	String name = view.getString("Enter Name", 2, 50);
+    	
+    	Director director = new Director(name);
+    	App.getDirectors().put(name.toLowerCase(), director);
+    	
+    	view.success(name + " saved successfully");
+    	dataChanged = true;
+    }
+    
+    private Menu createMenu()
+    {
+    	Menu menu = new Menu("Manage Directors");
+    	menu.addOption(new Option("Add New Director", 'A', this::add));
+    	menu.addOption(new Option("Edit Director", 'E', null));
+    	menu.addOption(new Option("Delete Director", 'D', null));
+    	menu.addOption(new Option("Search Directors", 'S', null));
+    	menu.addOption(new Option("View Director", 'V', null));
+    	menu.addOption(new Option("Back to Main", 'B', null));
+
+    	return menu;
+    }
+}
